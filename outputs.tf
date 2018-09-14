79b62343c24b46d5690046d443edc18052ef05da
Marco Capuccini <marco.capuccini@farmbio.uu.se>
2018-09-14 19:27:35 +0200

    Output ssh commands for tunnelling (#15)

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,13 @@ output "master_public_ip" {
   description = "Public IP address of the master node"
   value       = "${element(module.master.public_ip_list,0)}"
 }
+
+output "ssh_tunnel_zeppelin" {
+  description = "Apache Zeppelin tunnelling command"
+  value       = "ssh -N -f -L 8888:localhost:8888 core@${element(module.master.public_ip_list,0)}"
+}
+
+output "ssh_tunnel_sparkui" {
+  description = "Spark UI tunnelling command"
+  value       = "ssh -N -f -L 9999:localhost:9999 core@${element(module.master.public_ip_list,0)}"
+}
