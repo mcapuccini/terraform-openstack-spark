7d3c8adc856d06bff826d3bf70237c677731516d
Marco Capuccini <marco.capuccini@farmbio.uu.se>
2018-11-01 10:31:32 +0100

    Remove accidentally copied directory from root (#37)

--- ignition-hostname/output.tf
+++
@@ -1,4 +0,0 @@
-output "id" {
-  description = "Ignition hostname id list"
-  value       = ["${data.ignition_file.hostname.*.id}"]
-}
--- ignition-hostname/variables.tf
+++
@@ -1,7 +0,0 @@
-variable count {
-  description = "Number of ignition configurations to create"
-}
-
-variable hostname {
-  description = "Hostname to inject via Ignition (numerical postfix will be appended)"
-}
